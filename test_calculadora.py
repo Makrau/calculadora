40cdd8803ea0820f3b2da38ae4b2e21549e7eb5f
Makrau <iago006@hotmail.com>
2016-04-07 09:19:43 -0300

    Implementado teste de subtracao

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,3 +3,8 @@ from calculadora import ClassCalculadora
 def test_calculadora_adicao():
 	calc = ClassCalculadora()
 	assert calc.adicao(3,2) == 5
+
+def test_calculadora_subtracao():
+	calc = ClassCalculadora()
+	assert calc.subtracao(3,2) == 1
+
